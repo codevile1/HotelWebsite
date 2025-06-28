f50f9c5df86712c1b158f08697fcbca6a4313a5e
codevile1 <satyamchaurasiyaaa@gmail.com>
2025-06-28 18:51:10 +0530

    Add GitHub Pages deploy setup

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/HotelWebsite/', // 👈 IMPORTANT: this must match your repo name
   plugins: [react(),tailwindcss(),
 ],
 })
