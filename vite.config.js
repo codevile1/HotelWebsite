b9770a0564fcd79e4800bac778ef342742afaf6e
codevile1 <satyamchaurasiyaaa@gmail.com>
2025-06-29 09:28:54 +0530

    Fix: update Vite base path and ensure SPA routing with vercel.json

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/HotelWebsite/', // 👈 IMPORTANT: this must match your repo name
+  base: './', // 🔴 Add this line!
   plugins: [react(),tailwindcss(),
 ],
 })
